5880eebfccc5e019760d36a27f7e067b67ac23a0
Lee Dong Hyun <aopo3948@gmail.com>
2023-01-19 16:35:53 +0900

    c++ 예외처리
    try 로 예외 처리를 할 함수를 호출하면
    throw 로 예외를 발생시키고
    catch 문으로 발생한 예외를 처리함
    
    예외 발생 시 스택 풀기로 생성된 객체들을 해제 함
    
    생성자에서 예외 발생 시 소멸자가 호출되지 않음
    
    noexcept 함수에서 예외 발생 시 프로그램이 종료됨

---
+++ c++/11-0.cpp
@@ -0,0 +1,87 @@
+﻿#include <exception>
+#include "stdafx.h"
+
+template <typename T>
+class Vector {
+public:
+    Vector(size_t size) : size_(size) {
+        data_ = new T[size_];
+        for (int i = 0; i < size_; i++) {
+            data_[i] = 3;
+        }
+    }
+    ~Vector() { 
+        if(data_)
+            delete[] data_; 
+    }
+
+    const T& at(size_t index) const {
+        if (index >= size_) {
+            throw std::out_of_range("vector 의 index 가 범위를 초과하였습니다.");
+        }
+        return data_[index];
+    }
+
+private:
+    T* data_;
+    size_t size_;
+};
+
+class Resource {
+public:
+    Resource(int id) : m_id(id) {} 
+    ~Resource() { std::cout << "리소스 해제 : " << m_id << std::endl; }
+
+private:
+    int m_id;
+};
+
+int func3() {
+    Resource r(3);
+    throw std::runtime_error("Exception from 3!\n");
+}
+
+int func2() {
+    Resource r(2);
+    func3();
+    std::cout << "실행 안됨" << std::endl;
+    return 0;
+}
+
+int func1() {
+    Resource r(1);
+    func2();
+    std::cout << "실행 안됨" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    Vector<int> vec(3);
+
+    int i{}, data{};
+    std::cin >> i;
+
+    try {
+        data = vec.at(i);
+    }
+    catch (std::out_of_range& e) {
+        std::cout << "예외 발생 ! " << e.what() << std::endl;
+    }
+
+    std::cout << "읽은 데이터 : " << data << std::endl;
+
+    
+    
+    // 스택 풀기 확인
+    // 단, 생성자에서 예외 발생 시 소멸자 호출 X
+    std::cout << std::endl << std::endl;
+
+    try {
+        func1();
+    }
+    catch (std::exception& e) {
+        std::cout << "Exception : " << e.what();
+    }
+
+}
---
+++ c++/11-1.cpp
@@ -0,0 +1,68 @@
+﻿#include "stdafx.h"
+
+class Parent : public std::exception {
+public:
+	virtual const char* what() const noexcept override { return "Parent!\n"; }
+};
+
+class Child : public Parent {
+public:
+	const char* what() const noexcept override { return "Child!\n"; }
+};
+
+int func(int c) {
+	if (c == 1) {
+		throw Parent();
+	}
+	else if (c == 2) {
+		throw Child();
+	}
+	else if (c == 3) {
+		throw std::runtime_error("error");
+	}
+	return 0;
+}
+
+int NoExceptFunc() noexcept
+{
+	// noexcept 함수에서 예외처리 시 
+	// 런타임에서 프로그램이 종료됨
+
+	//throw std::runtime_error("error");
+	return 3;
+}
+
+int main()
+{
+	int c;
+	std::cin >> c;
+
+
+	// 부모클래스 catch 는 자식클래스 catch 뒤에 써야 함
+	// 반대로 쓸 경우 자식클래스를 throw 하더라도
+	// 부모클래스 Catch 문으로 들어감
+	try {
+		func(c);
+	}
+	catch (Child& c) {
+		std::cout << "Child Catch!" << std::endl;
+		std::cout << c.what();
+	}
+	catch (Parent& p) {
+		std::cout << "Parent Catch!" << std::endl;
+		std::cout << p.what();
+	}
+	catch (...) {
+		std::cout << "Default Catch" << std::endl;
+	}
+
+	int i{};
+	try {
+		i = NoExceptFunc();
+	}
+	catch(...){
+		std::cout << "Error" << std::endl;
+	}
+	
+	std::cout << i << std::endl;
+}
