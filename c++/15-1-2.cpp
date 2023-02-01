0cb409b19e6a57095aa0ef7ffeb41235a79711b0
Lee Dong Hyun <aopo3948@gmail.com>
2023-02-01 21:56:43 +0900

    쓰레드
    멀티 쓰레드 개요

---
+++ c++/15-1-2.cpp
@@ -0,0 +1,24 @@
+﻿#include <thread>
+#include "stdafx.h"
+
+void Worker(volatile int& counter)
+{
+	for (int i = 0; i < 10000; ++i)
+		counter += 1;
+}
+
+int main()
+{
+	int counter = 0;
+
+	std::vector<std::thread> workers;
+
+	for (int i = 0; i < 4; ++i)
+		workers.push_back(std::thread(Worker, std::ref(counter)));
+
+	for (int i = 0; i < 4; i++) {
+		workers[i].join();
+	}
+
+	std::cout << "Counter의 합 : " << counter << std::endl;
+}
---
+++ c++/15-1.cpp
@@ -0,0 +1,44 @@
+﻿#include <thread>
+#include <sstream>
+#include "stdafx.h"
+
+void Worker(std::vector<int>::iterator start, std::vector<int>::iterator end, int* result)
+{
+	int sum = 0;
+	for (auto itr = start; itr < end; ++itr)
+		sum += *itr;
+	*result = sum;
+
+	
+	std::stringstream thread_id;
+	thread_id << std::this_thread::get_id();
+
+	 std::cout << std::format("쓰레드 {} 에서 {} 부터 {} 까지 계산한 결과 : {} \n",
+		 thread_id.str(), *start, *(end - 1), sum);
+}
+
+int main()
+{
+	int data_size = 10000;
+
+	std::vector<int> data(data_size);
+	for (int i = 0; i < data_size; ++i)
+		data[i] = i;
+
+	std::vector<int> partial_sums(4);
+	
+	std::vector<std::thread> workers;
+	for (int i = 0; i < partial_sums.size(); ++i) {
+		workers.push_back(std::thread(Worker, data.begin() + i * (data_size / partial_sums.size()),
+			data.begin() + (i + 1) * (data_size / partial_sums.size()), &partial_sums[i]));
+	}
+
+	for (auto& th : workers)
+		th.join();
+
+	int total = 0;
+	for (int i = 0; i < partial_sums.size(); ++i)
+		total += partial_sums[i];
+
+	std::cout << "전체 합 : " << total << std::endl;
+}
